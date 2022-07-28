8f60186646dacb346e0548fdf04ffbc16ce83ddc
Dustin Martin <d_man_24_3@hotmail.com>
2022-07-27 21:43:16 -0500

    created tables in scheme.sql

---
+++ db/scheme.sql
@@ -0,0 +1,26 @@
+CREATE TABlE department {
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL
+};
+
+CREATE TABLE role {
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFRENCES department(id) 
+    ON DELETE CASCADE UPDATE CASCADE
+ 
+};
+
+CREATE TABLE employee {
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id) REFRENCES role(id) 
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (manager_id) REFRENCES employee(id) 
+    ON DELETE SET NULL ON UPDATE CASCADE
+};
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const db = require("./db/connections");
-//const apiRoutes = require('./routes/apiRoutes');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
